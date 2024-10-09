804e3dc69ff291b674838ded60791521c7596565
Mario Pringle <itspringle1213@gmail.com>
2024-10-09 12:53:54 -0700

    first commit

---
+++ fluid-sim/fluid-sim/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fluid-sim
+//
+//  Created by Mario Zuniga on 10/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fluid-sim/fluid-sim/fluid_simApp.swift
@@ -0,0 +1,17 @@
+//
+//  fluid_simApp.swift
+//  fluid-sim
+//
+//  Created by Mario Zuniga on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct fluid_simApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
